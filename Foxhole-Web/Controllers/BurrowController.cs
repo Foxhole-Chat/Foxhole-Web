668ea34cb1f36ca5142b09954e44ab5f2f184630
Killercat103 <real.killercat103@gmail.com>
2022-05-11 12:14:23 +0200

    Hello nightly!

--- Foxhole-Web/Controllers/BurrowController.cs
+++ Foxhole-Web/Controllers/BurrowController.cs
@@ -6,23 +6,8 @@ namespace Foxhole_Web.Controllers
 {
 	public class BurrowController : Controller
 	{
-		public async Task<IActionResult> Index(string id)
+		public IActionResult Index(string id)
 		{
-			RestClient rest_client = new(new RestClientOptions($"https://{Config.API_Host}/Burrow"));
-			RestRequest rest_request = new();
-
-			rest_request.AddHeader("Content-Type", "application/json");
-
-			rest_request.AddJsonBody
-			(
-				new
-				{
-					Burrow_ID = id.Replace('_', '/').Replace('-', '+')
-				}
-			);
-
-			RestResponse response = await rest_client.PostAsync(rest_request);
-
 			return View();
 		}
 	}
